a1b1f8268b5bdc4c2e0ad5fd0bb5fcd9ee1971c5
05satyam <satyam101905@gmail.com>
2024-02-29 21:15:24 -0800

    adding readme content and optimizing comments in source file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Dr.Thanos{CSU Fresno} and Satyam Mittal{CSU Fresno}',
     author_email='satyam101905@gmail.com',
     packages=find_packages(),
-    description='A library of non-convex optimization functions containing continuous and non-conitnuous functions implemented inn higher dimensions',
+    description='A library of non-convex optimization functions containing continuous and non-conitnuous functions implemented in higher dimensions',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
@@ -17,6 +17,9 @@ setup(
         'Intended Audience :: Researchers, Developers, Students',
         'Topic :: Software Development :: Libraries',
         'Programming Language :: Python :: 3',
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
         'License :: OSI Approved :: MIT License',
     ],
 )
